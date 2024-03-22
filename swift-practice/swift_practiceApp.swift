21af95e26caf0d39e0432deb5e4c7986054ab685
CrazyHuman00 <asakurashi00@icloud.com>
2024-03-22 21:22:30 +0900

    Initial Commit

---
+++ swift-practice/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  swift-practice
+//
+//  Created by 朝倉滉人 on 2024/03/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
